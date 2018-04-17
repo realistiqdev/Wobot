678c43bc129868891ae04e616d149c67f2bc4416
realistiqdev <ominousmatt@outlook.com>
2018-04-17 18:31:01 +0100

    Added Discord Requirement

--- commands/info.js
+++ commands/info.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 exports.run = (client, message, args) => {
   const embed = new Discord.MessageEmbed()
   .setAuthor('Bot Info', 'https://i.imgur.com/kDX3dso.png')
