d14d1fb08d048de8805ec4f32913fda5c99bf3d9
realistiqdev <ominousmatt@outlook.com>
2018-05-12 20:55:37 +0100

    Removed Eco

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
-
 const client = new Discord.Client();
 const prefix = '!';
 
@@ -12,16 +10,6 @@ client.on('message', message => {
   let sender = message.author;
   let args = message.content.slice(prefix.length).trim().split(' ');
   let cmd = args.shift().toLowerCase();
-
-  if(!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {};
-  if(!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 0;
-
-  fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
-    if (err) console.error(err);
-  })
-
-  if(!msg.startsWith(prefix)) return;
-  if(message.author.bot) return;
 
   try {
 
